b424a6396a5c55957906a56e4b8b4b6e305aad7f
Grisha Ghukasyan <ghukas_g@epitech.eu>
2015-11-25 14:40:40 +0100

    Add of auto generated main.c

---
+++ srcs/server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  R-Type
+//
+//  Created by Grisha Ghukasyan on 25/11/2015.
+//  Copyright © 2015 Grisha Ghukasyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
