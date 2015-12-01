0dc41efb497d9e77d653adc7c7c112e067110fc7
JeanGamain <jean.gamain@epitech.eu>
2015-12-01 16:03:10 +0100

    test sfml

--- srcs/client/test.cpp
+++ srcs/client/test.cpp
@@ -1,31 +1,24 @@
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+
 int main()
 {
-  // Create the main window
-  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-  // Load a sprite to display
-  sf::Texture texture;
-  if (!texture.loadFromFile("bra.png"))
-    return EXIT_FAILURE;
-  sf::Sprite sprite(texture);
-  // Start the game loop
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
   while (window.isOpen())
     {
-      // Process events
       sf::Event event;
       while (window.pollEvent(event))
         {
-	  // Close window: exit
 	  if (event.type == sf::Event::Closed)
 	    window.close();
         }
-      // Clear screen
+
       window.clear();
-      // Draw the sprite
-      window.draw(sprite);
-      // Update the window
+      window.draw(shape);
       window.display();
     }
-  return EXIT_SUCCESS;
+
+  return 0;
 }
