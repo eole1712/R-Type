1304e2d86682a7c5a737f1fdeca135d1f9edd23e
harnay_a <harnay_a@epitech.eu>
2015-12-06 22:10:54 +0100

    Ajout d'un MonsterTest à load dynamiquement

---
+++ srcs/server/core/monsters/MonsterTest.cpp
@@ -0,0 +1,61 @@
+#include <utility>
+#include "AUnit.hpp"
+#include "AMissile.hpp"
+#include "AMonster.hpp"
+#include "MonsterTest.hpp"
+
+// ébauche de classe pour test
+
+extern "C"
+{
+  Unit::Monster::AMonster*	NewMonster(unsigned int x, unsigned int y)
+  {
+    return (new Unit::Monster::MonsterTest(x, y));
+  }
+
+  void	DeleteMonster(Unit::Monster::AMonster* monster)
+  {
+    delete monster;
+  }
+}
+
+namespace Unit
+{
+
+namespace Monster
+{
+
+MonsterTest::MonsterTest(unsigned int x, unsigned int y)
+  : AMonster(1, x, y, std::make_pair(5, 5), Missile::BASIC)
+{}
+
+MonsterTest::~MonsterTest()
+{}
+
+Monster::type	MonsterTest::getMonsterType() const
+{
+  return (Monster::MONSTERTEST);
+}
+
+Missile::AMissile*	MonsterTest::shoot() const
+{
+  return (nullptr);
+}
+
+bool	MonsterTest::move()
+{
+  // todo + check en fonction de la map
+  this->_x -= 1;
+  return (1);
+}
+
+void	MonsterTest::getHit(AUnit* other)
+{
+  (void)other;
+  if (this->_hp > 0)
+    this->_hp -= 1;
+}
+
+}
+
+}
