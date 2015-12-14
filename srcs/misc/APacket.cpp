df2540c5de6e847cf4765da1c058126fdda81bb2
stefan_l <stefan_l@epitech.eu>
2015-12-14 14:58:08 +0100

    feat (network) ajout du Networker qui gère l'envoi /reception des paquets commun au server et au client

--- srcs/misc/APacket.cpp
+++ srcs/misc/APacket.cpp
@@ -1,36 +1,40 @@
 #include "APacket.hpp"
 
-	APacket::APacket(uint8_t type) {
-		_data.resize(kMaxSize);
-		_data.replace(sizeof(APacket::idSize),
-			sizeof(APacket::packetTypeSize),
-			reinterpret_cast<const char*>(&type),
-			sizeof(APacket::packetTypeSize));
-	}
+APacket::APacket(uint8_t type) {
+  _data.resize(kMaxSize);
+  _data.replace(sizeof(APacket::idSize),
+		sizeof(APacket::packetTypeSize),
+		reinterpret_cast<const char*>(&type),
+		sizeof(APacket::packetTypeSize));
+}
 
-	APacket::APacket(std::string const& other) : _data(other) {
+APacket::APacket(std::string const& other) : _data(other) {
 
-	}
+}
 
-	APacket::~APacket() {
+APacket::~APacket() {
 
-	}
+}
 
-	APacket::idSize APacket::getId() const {
-		return *reinterpret_cast<const APacket::idSize*>(_data.substr(0, sizeof(APacket::idSize)).c_str());
-	}
-	
-	uint8_t	APacket::getType() const {
-		return *reinterpret_cast<const APacket::packetTypeSize*>(_data.substr(sizeof(APacket::idSize), sizeof(APacket::packetTypeSize)).c_str());
-	}
+APacket::idSize APacket::getId() const {
+  return *reinterpret_cast<const APacket::idSize*>(_data.substr(0, sizeof(APacket::idSize)).c_str());
+}
 
-	std::string const& APacket::getData() const {
-		return _data;
-	}
+uint8_t	APacket::getType() const {
+  return sGetType(_data);
+}
 
-	void APacket::setId(APacket::idSize id) {
-		_data.replace(0,
-			sizeof(APacket::idSize),
-			reinterpret_cast<const char*>(&id),
-			sizeof(APacket::idSize));
-	}
+uint8_t APacket::sGetType(const std::string &data) {
+  return *reinterpret_cast<const APacket::packetTypeSize*>(data.substr(sizeof(APacket::idSize), sizeof(APacket::packetTypeSize)).c_str());
+}
+
+std::string const& APacket::getData() const {
+  return _data;
+}
+
+void APacket::setId(APacket::idSize id) {
+  _data.replace(0,
+		sizeof(APacket::idSize),
+		reinterpret_cast<const char*>(&id),
+		sizeof(APacket::idSize));
+}
--- srcs/misc/APacket.hpp
+++ srcs/misc/APacket.hpp
@@ -6,20 +6,21 @@
 
 class APacket {
 public:
-	typedef uint16_t idSize;
-	typedef uint8_t packetTypeSize;
+  typedef uint16_t idSize;
+  typedef uint8_t packetTypeSize;
 
-	static constexpr uint32_t kMaxSize = 512;
+  static constexpr uint32_t kMaxSize = 512;
 
-	APacket(uint8_t type);
-	APacket(std::string const& other);
-	virtual ~APacket();
+  APacket(uint8_t type);
+  APacket(std::string const& other);
+  virtual ~APacket();
 
-	uint16_t getId() const;
-	uint8_t	getType() const;
-	virtual std::string const& getData() const;
+  uint16_t		getId() const;
+  uint8_t		getType() const;
+  static uint8_t	sGetType(std::string const&);
+  virtual std::string const& getData() const;
 
-	void setId(uint16_t id);
+  void setId(uint16_t id);
 private:
 	std::string _data;
 };
