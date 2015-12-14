51c9b0b747b82cc378702fb49c33fbdfaa4dae63
Grisha Ghukasyan <ghukas_g@epitech.eu>
2015-12-14 15:49:49 +0100

    Implémentation de Move dans ABonus

--- srcs/server/core/ABonus.cpp
+++ srcs/server/core/ABonus.cpp
@@ -3,22 +3,27 @@
 
 namespace Unit
 {
-  
-  namespace Bonus
-  {
     
-    ABonus::ABonus(int x, int y, Unit::boxType hitBox, unsigned int id, unsigned int gameID)
-    : AUnit(1, Unit::ENEMY, x, y, hitBox, id, gameID)
-    {}
-    
-    ABonus::~ABonus()
-    {}
-    
-    Unit::type	ABonus::getType() const
+    namespace Bonus
     {
-      return (Unit::MISSILE);
+        
+        ABonus::ABonus(int x, int y, Unit::boxType hitBox, unsigned int id, unsigned int gameID)
+        : AUnit(1, Unit::ENEMY, x, y, hitBox, id, gameID)
+        {}
+        
+        ABonus::~ABonus()
+        {}
+        
+        Unit::type	ABonus::getType() const
+        {
+            return (Unit::MISSILE);
+        }
+        
+        pos      ABonus::move() const
+        {
+            return std::make_pair(_x, _y);
+        }
+
     }
     
-  }
-  
 }
