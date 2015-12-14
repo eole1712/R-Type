c951fea1243fb4bc244afa8b94feec2b09503c78
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2015-12-14 23:40:08 +0100

    monsterfactory client, monstertest client

---
+++ srcs/client/core/MonsterFactory.hpp
@@ -0,0 +1,43 @@
+#ifndef MONSTERFACTORY_HPP_
+# define MONSTERFACTORY_HPP_
+
+# include <list>
+# include <map>
+# include "AMonster.hpp"
+# include "Timer.hpp"
+
+typedef Unit::Monster::AMonster*	(*fptrNewMonster)(int x, int y, unsigned int id,
+							  Timer::time creationTime);
+typedef void				(*fptrDeleteMonster)(Unit::Monster::AMonster*);
+
+class ILibLoader;
+
+namespace Monster {
+
+    class Factory
+    {
+    private:
+        Factory();
+        Factory(std::map<Unit::Monster::type, std::string>);
+
+    public:
+        ~Factory();
+
+        Unit::Monster::AMonster*	createMonster(Unit::Monster::type, int x, int y,
+						      unsigned int id, Timer::time creationTime);
+
+        bool				addMonsterType(Unit::Monster::type, std::string libName);
+        bool				removeMonsterType(Unit::Monster::type);
+
+    private:
+        std::list<std::pair<Unit::Monster::type, ILibLoader*> >	_libs;
+
+    public:
+        static Factory*    getInstance();
+
+    private:
+        static Factory*     _instance;
+    };
+}
+
+#endif /* !MONSTERFACTORY_HPP_ */
