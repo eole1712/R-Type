f6018d841d00e6a599ac457b7de07735173a4e67
stefan_l <stefan_l@epitech.eu>
2015-12-14 15:43:40 +0100

    fix (packet) nouveau APacket pour -- merges

--- srcs/misc/APacket.hpp
+++ srcs/misc/APacket.hpp
@@ -9,30 +9,31 @@
 
 class APacket {
 public:
-	typedef uint16_t idSize;
-	typedef uint8_t packetTypeSize;
+  typedef uint16_t idSize;
+  typedef uint8_t packetTypeSize;
 
-	enum eType
-	{
-		SERVERCONNEXION,
-		CLIENTCONNEXION,
-		SERVERGAMEINFO,
-		CLIENTGAMEINFO
-	};
+  enum eType
+    {
+      SERVERCONNEXION,
+      CLIENTCONNEXION,
+      SERVERGAMEINFO,
+      CLIENTGAMEINFO
+    };
 
-	static constexpr uint32_t kMaxPacketSize = 512;
+  static constexpr uint32_t kMaxPacketSize = 512;
 
-	APacket(uint8_t type);
-	APacket(std::string const& other);
-	virtual ~APacket();
+  APacket(uint8_t type);
+  APacket(std::string const& other);
+  virtual ~APacket();
 
-	uint16_t getId() const;
-	uint8_t	getType() const;
-	virtual std::string const& getData() const;
+  uint16_t getId() const;
+  uint8_t	getType() const;
+  virtual std::string const& getData() const;
+  static uint8_t sGetType(std::string const&);
 
-	void setId(uint16_t id);
+  void setId(uint16_t id);
 protected:
-	std::string _data;
+  std::string _data;
 };
 
 #endif /* !APACKET_H_ */
