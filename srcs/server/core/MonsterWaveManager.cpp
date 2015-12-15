f48f88d6c5b4e42e3a029b66b20c33fb29d38533
Grisha Ghukasyan <ghukas_g@epitech.eu>
2015-12-15 12:27:11 +0100

    Mise a jour de la façon dont les waves sont ajoutés pendant la game tous les X intervalles.

--- srcs/server/core/MonsterWaveManager.cpp
+++ srcs/server/core/MonsterWaveManager.cpp
@@ -3,72 +3,75 @@
 #include "Map.hpp"
 
 namespace Monster {
-  
-  WaveManager::WaveManager(IMap *map, unsigned int gameID)
-  : _map(map), _list(), _timer(0), _gameID(gameID)
-  {
-  }
-  
-  WaveManager::~WaveManager()
-  {
-    std::for_each(_list.begin(), _list.end(), [](Wave* wave){
-      if (wave) {
-        delete wave;
-      }});
-  }
-  
-  Wave*     WaveManager::ClassicWave(unsigned int gameID)
-  {
-    Wave*   wave = new Wave(gameID);
     
-    for (int j = 0; j < 3; j++) {
-      for (int i = 0; i < 3; i++) {
-        wave->addMonster(Unit::Monster::MONSTERTEST, Map::WIDTH - 1, i * (Map::HEIGHT / 3));
-      }
-      wave->addWaitingTime(2);
-      wave->addMonster(Unit::Monster::MONSTERTEST, Map::WIDTH - 1, Map::HEIGHT / 2);
-      wave->addWaitingTime(2);
-    }
-    return wave;
-  }
-  
-  void    WaveManager::execConfig(Timer &time)
-  {
-    if ((time.getElapsedTime() % 10000) == 0)
+    WaveManager::WaveManager(IMap *map, unsigned int gameID)
+    : _map(map), _list(), _timer(0), _gameID(gameID)
     {
-      addWave(Monster::WaveManager::ClassicWave(_gameID));
     }
-  }
-  
-  void    WaveManager::addWave(Wave* wave)
-  {
-    _list.push_back(wave);
-  }
-  
-  void    WaveManager::removeWave(Wave* wave)
-  {
-    if (wave)
+    
+    WaveManager::~WaveManager()
     {
-      _list.remove(wave);
-      delete wave;
+        std::for_each(_list.begin(), _list.end(), [](Wave* wave){
+            if (wave) {
+                delete wave;
+            }});
     }
-  }
-  
-  void    WaveManager::nextAction()
-  {
-    std::for_each(_list.begin(), _list.end(), [this](Wave *wave){
-
-      Unit::AUnit*    unit;
-      while (wave->isFinished() && (unit = wave->getNextMonster()))
-        {
-          this->_map->addUnit(unit);
+    
+    Wave*     WaveManager::ClassicWave(unsigned int gameID)
+    {
+        Wave*   wave = new Wave(gameID);
+        
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 3; i++) {
+                wave->addMonster(Unit::Monster::MONSTERTEST, Map::WIDTH - 1, i * (Map::HEIGHT / 3));
+            }
+            wave->addWaitingTime(2);
+            wave->addMonster(Unit::Monster::MONSTERTEST, Map::WIDTH - 1, Map::HEIGHT / 2);
+            wave->addWaitingTime(2);
         }
-    });
-    for (std::list<Wave*>::iterator it = _list.begin(); it != _list.end(); it++)
-    {
-      if ((*it)->isEmpty())
-        it = _list.erase(it);
+        return wave;
     }
-  }
-  
+    
+    void    WaveManager::execConfig(Timer &time)
+    {
+        int t[] = {1};
+        
+        if (time.getElapsedTime() > (10000 * t[0]))
+        {
+            addWave(Monster::WaveManager::ClassicWave(_gameID));
+            t[0]++;
+        }
+    }
+    
+    void    WaveManager::addWave(Wave* wave)
+    {
+        _list.push_back(wave);
+    }
+    
+    void    WaveManager::removeWave(Wave* wave)
+    {
+        if (wave)
+        {
+            _list.remove(wave);
+            delete wave;
+        }
+    }
+    
+    void    WaveManager::nextAction()
+    {
+        std::for_each(_list.begin(), _list.end(), [this](Wave *wave){
+            
+            Unit::AUnit*    unit;
+            while (wave->isFinished() && (unit = wave->getNextMonster()))
+            {
+                this->_map->addUnit(unit);
+            }
+        });
+        for (std::list<Wave*>::iterator it = _list.begin(); it != _list.end(); it++)
+        {
+            if ((*it)->isEmpty())
+                it = _list.erase(it);
+        }
+    }
+    
 }
