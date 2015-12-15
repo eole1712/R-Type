363f151d584c7814db43bcb55713d4c28fbaf0e0
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2015-12-15 22:17:20 +0100

    ajout du defined apple dans le main de test compil libloader

--- srcs/common/main.cpp
+++ srcs/common/main.cpp
@@ -1,6 +1,6 @@
 #include "ILibLoader.hpp"
 
-#if defined(__linux__)
+#if defined(__linux__) || (defined __APPLE__)
 #include "linux_specific/CULibLoader.hpp"
 #elif defined(_WIN32)
 #include "windows_specific/CWLibLoader.hpp"
@@ -10,7 +10,7 @@ int	main()
 {
   ILibLoader*	libLoader;
 
-#if defined(__linux__)
+#if defined(__linux__) || (defined __APPLE__)
   libLoader = new CULibLoader("MonsterTest", "NewMonster", "DeleteMonster");
 #elif defined(_WIN32)
   libLoader = new CWLibLoader("MonsterTest", "NewMonster", "DeleteMonster");
