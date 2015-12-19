fba2af04b66319428b6e73b26f167f57609789b9
jean.gamain <jean.gamain@epitech.eu>
2015-12-19 16:20:09 +0100

    port par defaut client 6137

--- srcs/client/core/main.cpp
+++ srcs/client/core/main.cpp
@@ -3,13 +3,9 @@
 
 int	main(int ac, char **av)
 {
-  if (ac == 2)
-    {
-      Client cl(std::atoi(av[1]));
-      cl.start();
-      Menu menu(720, 480, &cl);
-    }
+  int port = (ac > 1) ? std::atoi(av[1]) : 6137;
+  Client cl(port);
 
-  //  menu.initMainView();
-
+  cl.start();
+  Menu menu(720, 480, &cl);
 }
